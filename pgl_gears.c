b41630e13f050a60689361e670d4c22f9569d2eb
Nicolas Caramelli <caramelli.devel@gmail.com>
2023-07-08 18:31:47 +0200

    add PortableGL support

--- pgl_gears.c
+++ pgl_gears.c
@@ -21,15 +21,8 @@
   THE SOFTWARE.
 */
 
-#include GLESV2_H
-#include <dlfcn.h>
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include PGL_H
 #include "engine.h"
-
-#include "image_loader.h"
 
 extern struct list engine_list;
 
@@ -122,7 +115,7 @@ static void invert(float *a)
 #define GEAR1 1
 #define GEAR2 2
 
-typedef float Vertex[8];
+typedef float Vertex[6];
 
 typedef struct {
   int begin;
@@ -138,41 +131,6 @@ struct gear {
 };
 
 struct gears {
-  void *lib_handle;
-  void           (*glAttachShader)(GLuint, GLuint);
-  void           (*glBindAttribLocation)(GLuint, GLuint, const GLchar *);
-  void           (*glBindBuffer)(GLenum, GLuint);
-  void           (*glBufferData)(GLenum, GLsizeiptr, const GLvoid *, GLenum);
-  void           (*glClear)(GLbitfield);
-  void           (*glClearColor)(GLfloat, GLfloat, GLfloat, GLfloat);
-  void           (*glCompileShader)(GLuint);
-  GLuint         (*glCreateProgram)();
-  GLuint         (*glCreateShader)(GLenum);
-  void           (*glDeleteBuffers)(GLsizei, const GLuint *);
-  void           (*glDeleteProgram)(GLuint);
-  void           (*glDeleteShader)(GLuint);
-  void           (*glDisableVertexAttribArray)(GLuint);
-  void           (*glDrawArrays)(GLenum, GLint, GLsizei);
-  void           (*glEnable)(GLenum);
-  void           (*glEnableVertexAttribArray)(GLuint);
-  void           (*glGenBuffers)(GLsizei, GLuint *);
-  GLenum         (*glGetError)();
-  void           (*glGetProgramInfoLog)(GLuint, GLsizei, GLsizei *, GLchar *);
-  void           (*glGetProgramiv)(GLuint, GLenum, GLint *);
-  void           (*glGetShaderInfoLog)(GLuint, GLsizei, GLsizei *, GLchar *);
-  void           (*glGetShaderiv)(GLuint, GLenum, GLint *);
-  const GLubyte *(*glGetString)(GLenum);
-  int            (*glGetUniformLocation)(GLuint, const GLchar *);
-  void           (*glLinkProgram)(GLuint);
-  void           (*glShaderSource)(GLuint, GLsizei, const GLchar **, const GLint *);
-  void           (*glUniform1i)(GLint, GLint);
-  void           (*glUniform4fv)(GLint, GLsizei, const GLfloat *);
-  void           (*glUniformMatrix4fv)(GLint, GLsizei, GLboolean, const GLfloat *);
-  void           (*glUseProgram)(GLuint);
-  void           (*glTexImage2D)(GLenum, GLint, GLint, GLsizei, GLsizei, GLint, GLenum, GLenum, const GLvoid *);
-  void           (*glTexParameteri)(GLenum, GLenum, GLint);
-  void           (*glVertexAttribPointer)(GLuint, GLint, GLenum, GLboolean, GLsizei, const GLvoid *);
-  void           (*glViewport)(GLint, GLint, GLsizei, GLsizei);
   GLuint program;
   struct gear *gear[3];
   float Projection[16];
@@ -188,7 +146,7 @@ static void delete_gear(gears_t *gears, int id)
   }
 
   if (gear->vbo) {
-    gears->glDeleteBuffers(1, &gear->vbo);
+    glDeleteBuffers(1, &gear->vbo);
   }
   if (gear->strips) {
     free(gear->strips);
@@ -207,7 +165,7 @@ static int create_gear(gears_t *gears, int id, float inner, float outer, float w
   struct gear *gear;
   float r0, r1, r2, da, a1, ai, s[5], c[5];
   int i, j;
-  float n[3], t[2];
+  float n[3];
   int k = 0;
   GLenum err = GL_NO_ERROR;
 
@@ -244,10 +202,6 @@ static int create_gear(gears_t *gears, int id, float inner, float outer, float w
     n[1] = ny; \
     n[2] = nz;
 
-  #define texcoord(tx, ty) \
-    t[0] = tx; \
-    t[1] = ty;
-
   #define vertex(x, y, z) \
     gear->vertices[gear->nvertices][0] = x; \
     gear->vertices[gear->nvertices][1] = y; \
@@ -255,8 +209,6 @@ static int create_gear(gears_t *gears, int id, float inner, float outer, float w
     gear->vertices[gear->nvertices][3] = n[0]; \
     gear->vertices[gear->nvertices][4] = n[1]; \
     gear->vertices[gear->nvertices][5] = n[2]; \
-    gear->vertices[gear->nvertices][6] = t[0]; \
-    gear->vertices[gear->nvertices][7] = t[1]; \
     gear->nvertices++;
 
   for (i = 0; i < teeth; i++) {
@@ -270,21 +222,13 @@ static int create_gear(gears_t *gears, int id, float inner, float outer, float w
     /* front face normal */
     normal(0, 0, 1);
     /* front face vertices */
-    texcoord(0.36 * r2 * s[1] / r1 + 0.5, 0.36 * r2 * c[1] / r1 + 0.5);
     vertex(r2 * c[1], r2 * s[1], width / 2);
-    texcoord(0.36 * r2 * s[2] / r1 + 0.5, 0.36 * r2 * c[2] / r1 + 0.5);
     vertex(r2 * c[2], r2 * s[2], width / 2);
-    texcoord(0.36 * r1 * s[0] / r1 + 0.5, 0.36 * r1 * c[0] / r1 + 0.5);
     vertex(r1 * c[0], r1 * s[0], width / 2);
-    texcoord(0.36 * r1 * s[3] / r1 + 0.5, 0.36 * r1 * c[3] / r1 + 0.5);
     vertex(r1 * c[3], r1 * s[3], width / 2);
-    texcoord(0.36 * r0 * s[0] / r1 + 0.5, 0.36 * r0 * c[0] / r1 + 0.5);
     vertex(r0 * c[0], r0 * s[0], width / 2);
-    texcoord(0.36 * r1 * s[4] / r1 + 0.5, 0.36 * r1 * c[4] / r1 + 0.5);
     vertex(r1 * c[4], r1 * s[4], width / 2);
-    texcoord(0.36 * r0 * s[4] / r1 + 0.5, 0.36 * r0 * c[4] / r1 + 0.5);
     vertex(r0 * c[4], r0 * s[4], width / 2);
-    texcoord(0, 0);
     /* front face end */
     gear->strips[k].count = 7;
     k++;
@@ -373,21 +317,21 @@ static int create_gear(gears_t *gears, int id, float inner, float outer, float w
 
   /* vertex buffer object */
 
-  gears->glGenBuffers(1, &gear->vbo);
+  glGenBuffers(1, &gear->vbo);
   if (!gear->vbo) {
     printf("glGenBuffers failed\n");
     goto out;
   }
 
-  gears->glBindBuffer(GL_ARRAY_BUFFER, gear->vbo);
-  err = gears->glGetError();
+  glBindBuffer(GL_ARRAY_BUFFER, gear->vbo);
+  err = glGetError();
   if (err) {
     printf("glBindBuffer failed: 0x%x\n", err);
     goto out;
   }
 
-  gears->glBufferData(GL_ARRAY_BUFFER, gear->nvertices * sizeof(Vertex), gear->vertices, GL_STATIC_DRAW);
-  err = gears->glGetError();
+  glBufferData(GL_ARRAY_BUFFER, gear->nvertices * sizeof(Vertex), gear->vertices, GL_STATIC_DRAW);
+  err = glGetError();
   if (err) {
     printf("glBufferData failed: 0x%x\n", err);
     goto out;
@@ -400,20 +344,26 @@ out:
   return -1;
 }
 
+typedef struct Uniforms {
+  vec4 LightPos;
+  mat4 ModelViewProjectionMatrix;
+  mat4 NormalMatrix;
+  vec4 Color;
+} Uniforms;
+
 static void draw_gear(gears_t *gears, int id, float model_tx, float model_ty, float model_rz, const float *color)
 {
   struct gear *gear = gears->gear[id];
   const float pos[4] = { 5.0, 5.0, 10.0, 0.0 };
   float ModelView[16], ModelViewProjection[16];
-  GLint LightPos_loc, ModelViewProjection_loc, Normal_loc, Color_loc, TextureEnable_loc;
+  Uniforms uniforms;
   int k;
 
   if (!gear) {
     return;
   }
 
-  LightPos_loc = gears->glGetUniformLocation(gears->program, "u_LightPos");
-  gears->glUniform4fv(LightPos_loc, 1, pos);
+  memcpy(&uniforms.LightPos, pos, sizeof(vec4));
 
   memcpy(ModelView, gears->View, sizeof(ModelView));
 
@@ -422,45 +372,35 @@ static void draw_gear(gears_t *gears, int id, float model_tx, float model_ty, fl
 
   memcpy(ModelViewProjection, gears->Projection, sizeof(ModelViewProjection));
   multiply(ModelViewProjection, ModelView);
-  ModelViewProjection_loc = gears->glGetUniformLocation(gears->program, "u_ModelViewProjectionMatrix");
-  gears->glUniformMatrix4fv(ModelViewProjection_loc, 1, GL_FALSE, ModelViewProjection);
+  memcpy(&uniforms.ModelViewProjectionMatrix, ModelViewProjection, sizeof(mat4));
 
   invert(ModelView);
   transpose(ModelView);
-  Normal_loc = gears->glGetUniformLocation(gears->program, "u_NormalMatrix");
-  gears->glUniformMatrix4fv(Normal_loc, 1, GL_FALSE, ModelView);
+  memcpy(&uniforms.NormalMatrix, ModelView, sizeof(mat4));
 
-  Color_loc = gears->glGetUniformLocation(gears->program, "u_Color");
-  gears->glUniform4fv(Color_loc, 1, color);
+  memcpy(&uniforms.Color, color, sizeof(vec4));
 
-  TextureEnable_loc = gears->glGetUniformLocation(gears->program, "u_TextureEnable");
-  if (getenv("NO_TEXTURE"))
-    gears->glUniform1i(TextureEnable_loc, 0);
-  else
-    gears->glUniform1i(TextureEnable_loc, 1);
+  pglSetUniform(&uniforms);
 
-  gears->glBindBuffer(GL_ARRAY_BUFFER, gear->vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, gear->vbo);
 
-  gears->glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), NULL);
-  gears->glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const float *)NULL + 3);
-  gears->glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const float *)NULL + 6);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 3 * sizeof(float));
 
-  gears->glEnableVertexAttribArray(0);
-  gears->glEnableVertexAttribArray(1);
-  gears->glEnableVertexAttribArray(2);
+  glEnableVertexAttribArray(0);
+  glEnableVertexAttribArray(1);
 
   for (k = 0; k < gear->nstrips; k++) {
-    gears->glDrawArrays(GL_TRIANGLE_STRIP, gear->strips[k].begin, gear->strips[k].count);
+    glDrawArrays(GL_TRIANGLE_STRIP, gear->strips[k].begin, gear->strips[k].count);
   }
 
-  gears->glDisableVertexAttribArray(2);
-  gears->glDisableVertexAttribArray(1);
-  gears->glDisableVertexAttribArray(0);
+  glDisableVertexAttribArray(1);
+  glDisableVertexAttribArray(0);
 }
 
 /******************************************************************************/
 
-static void glesv2_gears_term(gears_t *gears)
+static void pgl_gears_term(gears_t *gears)
 {
   if (!gears) {
     return;
@@ -476,35 +416,44 @@ static void glesv2_gears_term(gears_t *gears)
     delete_gear(gears, GEAR0);
   }
   if (gears->program) {
-    gears->glDeleteProgram(gears->program);
+    glDeleteProgram(gears->program);
   }
 
-  printf("%s\n", gears->glGetString(GL_VERSION));
-  printf("%s\n", gears->glGetString(GL_SHADING_LANGUAGE_VERSION));
-
-  if (gears->lib_handle) {
-    dlclose(gears->lib_handle);
-  }
+  printf("%s\n", glGetString(GL_VERSION));
 
   free(gears);
 }
 
-static gears_t *glesv2_gears_init(int win_width, int win_height)
+#define POSITION 0
+#define NORMAL   1
+#define COLOR    0
+
+void vertex_shader(float *vs_output, void *vertex_attribs, Shader_Builtins *builtins, void *uniforms)
+{
+  vec4 *v = (vec4 *)vs_output;
+  vec4 *a = vertex_attribs;
+  Uniforms *u = uniforms;
+
+  builtins->gl_Position = mult_mat4_vec4(u->ModelViewProjectionMatrix, a[POSITION]);
+  vec4 l = { 0.2, 0.2, 0.2, 1 };
+  vec3 L = { u->LightPos.x, u->LightPos.y, u->LightPos.z };
+  vec4 n = mult_mat4_vec4(u->NormalMatrix, a[NORMAL]);
+  vec3 N = { n.x, n.y, n.z };
+  float dot = dot_vec3s(norm_vec3(L), norm_vec3(N));
+  v[COLOR] = add_vec4s(mult_vec4s(u->Color, l), scale_vec4(u->Color, MAX(dot, 0.0)));
+}
+
+void fragment_shader(float *fs_input, Shader_Builtins *builtins, void *uniforms)
+{
+  vec4 *v = (vec4 *)fs_input;
+
+  builtins->gl_FragColor = v[COLOR];
+}
+
+static gears_t *pgl_gears_init(int win_width, int win_height)
 {
   gears_t *gears = NULL;
-  const char vertShaderSource[] = {
-    #include "vert.xxd"
-  };
-  const char fragShaderSource[] = {
-    #include "frag.xxd"
-  };
-  const GLchar *code;
-  GLint params;
-  GLchar *log;
-  GLuint vertShader = 0;
-  GLuint fragShader = 0;
-  int texture_width, texture_height;
-  void *texture_data = NULL;
+  GLenum interpolation[3] = { SMOOTH, SMOOTH, SMOOTH };
   const float zNear = 5, zFar = 60;
 
   gears = calloc(1, sizeof(gears_t));
@@ -513,174 +462,21 @@ static gears_t *glesv2_gears_init(int win_width, int win_height)
     return NULL;
   }
 
-  gears->lib_handle = dlopen(GLESV2_LIB, RTLD_LAZY);
-  if (!gears->lib_handle) {
-    printf("%s library not found\n", GLESV2_LIB);
-    goto out;
-  }
+  glEnable(GL_DEPTH_TEST);
 
-  #define DLSYM(sym) gears->sym = dlsym(gears->lib_handle, #sym); \
-  if (!gears->sym) { \
-    printf("%s not found\n", #sym); \
-    goto out; \
-  }
-
-  DLSYM(glAttachShader);
-  DLSYM(glBindAttribLocation);
-  DLSYM(glBindBuffer);
-  DLSYM(glBufferData);
-  DLSYM(glClear);
-  DLSYM(glClearColor);
-  DLSYM(glCompileShader);
-  DLSYM(glCreateProgram);
-  DLSYM(glCreateShader);
-  DLSYM(glDeleteBuffers);
-  DLSYM(glDeleteShader);
-  DLSYM(glDeleteProgram);
-  DLSYM(glDisableVertexAttribArray);
-  DLSYM(glDrawArrays);
-  DLSYM(glEnable);
-  DLSYM(glEnableVertexAttribArray);
-  DLSYM(glGenBuffers);
-  DLSYM(glGetError);
-  DLSYM(glGetProgramInfoLog);
-  DLSYM(glGetProgramiv);
-  DLSYM(glGetShaderInfoLog);
-  DLSYM(glGetShaderiv);
-  DLSYM(glGetString);
-  DLSYM(glGetUniformLocation);
-  DLSYM(glLinkProgram);
-  DLSYM(glShaderSource);
-  DLSYM(glUniform1i);
-  DLSYM(glUniform4fv);
-  DLSYM(glUniformMatrix4fv);
-  DLSYM(glUseProgram);
-  DLSYM(glTexImage2D);
-  DLSYM(glTexParameteri);
-  DLSYM(glVertexAttribPointer);
-  DLSYM(glViewport);
-
-  gears->glEnable(GL_DEPTH_TEST);
-
-  gears->program = gears->glCreateProgram();
+  gears->program = pglCreateProgram(vertex_shader, fragment_shader, 3, interpolation, GL_FALSE);
   if (!gears->program) {
     printf("glCreateProgram failed\n");
     goto out;
   }
 
-  /* vertex shader */
-
-  vertShader = gears->glCreateShader(GL_VERTEX_SHADER);
-  if (!vertShader) {
-    printf("glCreateShader vertex failed\n");
-    goto out;
-  }
-
-  code = vertShaderSource;
-  gears->glShaderSource(vertShader, 1, &code, NULL);
-
-  gears->glCompileShader(vertShader);
-  gears->glGetShaderiv(vertShader, GL_COMPILE_STATUS, &params);
-  if (!params) {
-    gears->glGetShaderiv(vertShader, GL_INFO_LOG_LENGTH, &params);
-    log = calloc(1, params);
-    if (!log) {
-      printf("calloc log failed\n");
-      goto out;
-    }
-    gears->glGetShaderInfoLog(vertShader, params, NULL, log);
-    printf("glCompileShader vertex failed: %s", log);
-    free(log);
-    goto out;
-  }
-
-  gears->glAttachShader(gears->program, vertShader);
-
-  /* fragment shader */
-
-  fragShader = gears->glCreateShader(GL_FRAGMENT_SHADER);
-  if (!fragShader) {
-    printf("glCreateShader fragment failed\n");
-    goto out;
-  }
-
-  code = fragShaderSource;
-  if (strstr((char *)gears->glGetString(GL_SHADING_LANGUAGE_VERSION), "1.20") ||
-      strstr((char *)gears->glGetString(GL_SHADING_LANGUAGE_VERSION), "1.30")) {
-    code += strlen("precision mediump float;\n");
-  }
-  gears->glShaderSource(fragShader, 1, &code, NULL);
-
-  gears->glCompileShader(fragShader);
-  gears->glGetShaderiv(fragShader, GL_COMPILE_STATUS, &params);
-  if (!params) {
-    gears->glGetShaderiv(fragShader, GL_INFO_LOG_LENGTH, &params);
-    log = calloc(1, params);
-    if (!log) {
-      printf("calloc log failed\n");
-      goto out;
-    }
-    gears->glGetShaderInfoLog(fragShader, params, NULL, log);
-    printf("glCompileShader fragment failed: %s", log);
-    free(log);
-    goto out;
-  }
-
-  gears->glAttachShader(gears->program, fragShader);
-
-  /* link and use program */
-
-  gears->glBindAttribLocation(gears->program, 0, "a_Position");
-  gears->glBindAttribLocation(gears->program, 1, "a_Normal");
-  gears->glBindAttribLocation(gears->program, 2, "a_TexCoord");
-
-  gears->glLinkProgram(gears->program);
-  gears->glGetProgramiv(gears->program, GL_LINK_STATUS, &params);
-  if (!params) {
-    gears->glGetProgramiv(gears->program, GL_INFO_LOG_LENGTH, &params);
-    log = calloc(1, params);
-    if (!log) {
-      printf("calloc log failed\n");
-      goto out;
-    }
-    gears->glGetProgramInfoLog(gears->program, params, NULL, log);
-    printf("glLinkProgram failed: %s", log);
-    free(log);
-    goto out;
-  }
-
-  /* destory shaders */
-
-  gears->glDeleteShader(fragShader);
-  gears->glDeleteShader(vertShader);
-  vertShader = fragShader = 0;
-
-  /* load texture */
-
-  image_load(getenv("TEXTURE"), NULL, &texture_width, &texture_height);
-
-  texture_data = malloc(texture_width * texture_height * 4);
-  if (!texture_data) {
-    printf("malloc texture_data failed\n");
-    goto out;
-  }
-
-  image_load(getenv("TEXTURE"), texture_data, &texture_width, &texture_height);
-
-  gears->glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texture_width, texture_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, texture_data);
-
-  free(texture_data);
-
-  gears->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  gears->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
   /* install program, set clear values, set viewport */
 
-  gears->glUseProgram(gears->program);
+  glUseProgram(gears->program);
 
-  gears->glClearColor(0, 0, 0, 1);
+  glClearColor(0, 0, 0, 1);
 
-  gears->glViewport(0, 0, win_width, win_height);
+  glViewport(0, 0, win_width, win_height);
 
   /* create gears */
 
@@ -706,17 +502,11 @@ static gears_t *glesv2_gears_init(int win_width, int win_height)
   return gears;
 
 out:
-  if (fragShader) {
-    gears->glDeleteShader(fragShader);
-  }
-  if (vertShader) {
-    gears->glDeleteShader(vertShader);
-  }
-  glesv2_gears_term(gears);
+  pgl_gears_term(gears);
   return NULL;
 }
 
-static void glesv2_gears_draw(gears_t *gears, float view_tz, float view_rx, float view_ry, float model_rz)
+static void pgl_gears_draw(gears_t *gears, float view_tz, float view_rx, float view_ry, float model_rz)
 {
   const float red[4] = { 0.8, 0.1, 0.0, 1.0 };
   const float green[4] = { 0.0, 0.8, 0.2, 1.0 };
@@ -726,7 +516,7 @@ static void glesv2_gears_draw(gears_t *gears, float view_tz, float view_rx, floa
     return;
   }
 
-  gears->glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
   identity(gears->View);
   translate(gears->View, 0, 0, view_tz);
@@ -740,15 +530,15 @@ static void glesv2_gears_draw(gears_t *gears, float view_tz, float view_rx, floa
 
 /******************************************************************************/
 
-static engine_t glesv2_engine = {
-  "glesv2",
+static engine_t pgl_engine = {
+  "pgl",
   2,
-  glesv2_gears_init,
-  glesv2_gears_draw,
-  glesv2_gears_term
+  pgl_gears_init,
+  pgl_gears_draw,
+  pgl_gears_term
 };
 
 static void __attribute__((constructor)) engine_ctor()
 {
-  list_add(&glesv2_engine.entry, &engine_list);
+  list_add(&pgl_engine.entry, &engine_list);
 }
